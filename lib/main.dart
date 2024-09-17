5707a4cf7b7f6c75582d773af544fc4885cdbf8d
basanthraj15 <basanthraj.2002@gmail.com>
2024-09-17 23:08:03 +0530

    Water intake ui completed

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,131 @@ class _WaterIntakeHomePageState extends State<WaterIntakeHomePage> {
     setState(() {
       _waterIntake++;
       pref.setInt("waterIntake", _waterIntake);
+      if (_waterIntake >= _dailyGoal) {
+        showGoalReachedDialog();
+      }
     });
+  }
+
+  //reset goal
+
+  Future<void> _resetWaterIntake() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    setState(() {
+      _waterIntake = 0;
+      //update in ui as well
+      pref.setInt("waterIntake", _waterIntake);
+    });
+  }
+
+  //set goal
+
+  Future<void> setDailyGoal(int newGoal) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      _dailyGoal = newGoal;
+      pref.setInt("dailyGoal", newGoal);
+    });
+  }
+
+  Future<void> showGoalReachedDialog() async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Congratulations"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text(
+                      "You have reached your daily Goal of $_dailyGoal glasses")
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"))
+            ],
+          );
+        });
+  }
+
+  Future<void> showResetConfirmationDialog() async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Reset Water Intake"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text("Are you sure you want to reset your water intake?")
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Cancel")),
+              TextButton(
+                  onPressed: () {
+                    _resetWaterIntake();
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Reset"))
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 12, 3, 52),
+        title: Text(
+          "Water Intake App",
+          style: TextStyle(color: Colors.grey),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Icon(
+                Icons.water_drop_outlined,
+                size: 110,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "You have consumed:",
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "$_waterIntake glasses of water",
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
